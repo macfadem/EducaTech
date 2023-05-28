396b04a7bce2ffb26e4ebf3aaa3a152b32c53314
Kevin Macfadem <k_macfadem@hotmail.com>
2023-05-28 16:55:28 -0300

    header ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ParceirosComponent } from './pages/parceiros/parceiros.component';
+import { DoeComponent } from './pages/doe/doe.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'sobre', component: AboutComponent },
+  { path: 'parceiros', component: ParceirosComponent },
+  { path: 'doe', component: DoeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
