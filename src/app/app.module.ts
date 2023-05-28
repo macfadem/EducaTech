f3d9ae1671a210bec287110836b5fee9b04f6356
Kevin Macfadem <k_macfadem@hotmail.com>
2023-05-28 16:34:41 -0300

    base pronta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ParceirosComponent } from './pages/parceiros/parceiros.component';
+import { DoeComponent } from './pages/doe/doe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ParceirosComponent,
+    DoeComponent
   ],
   imports: [
     BrowserModule,
